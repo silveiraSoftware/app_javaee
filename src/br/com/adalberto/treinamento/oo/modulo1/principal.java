7379683258e72303fa8dc796d20a3351e6583c0d
adalbertosn1982 <adalbertosn1982@gmail.com>
2020-04-23 00:34:07 -0300

    primiero commit do projeto

---
+++ src/br/com/adalberto/treinamento/oo/modulo1/principal.java
@@ -0,0 +1,10 @@
+package br.com.adalberto.treinamento.oo.modulo1;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ok");
+	}
+
+}
